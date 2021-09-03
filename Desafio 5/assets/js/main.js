a306162b471ce6484b9b3495acc0e455cffb54cb
Aldovani <aldovane.etec@gmail.com>
2021-09-03 20:19:41 -0300

    finalização desafio 5

--- Desafio 5/assets/js/main.js
+++ Desafio 5/assets/js/main.js
@@ -1,21 +1,29 @@
+const $audio = document.querySelector("[data-js='music']");
 const $modal = document.querySelector('[data-js="modal"]');
 const $button = document.querySelector('[data-js="button-modal"]');
-const $iframe = document.querySelector("iframe");
+const $video = document.querySelector("video");
+
+$audio.volume=0.03
 
 $button.addEventListener("click", () => {
   $modal.classList.add("hide");
-  $iframe.src = "https://www.youtube.com/embed/SgZI655GgHk";
+  $audio.pause();
 });
 
 $modal.addEventListener("click", (e) => {
-  if (e.currentTarget) {
+  if (e.target.classList.value == "modal hide") {
     $modal.classList.remove("hide");
-    $iframe.src = "";
+    $video.pause();
+    $video.currentTime=0
+    $audio.play();
   }
 });
 
 window.addEventListener("keyup", (e) => {
   if (e.code == "Escape" && $modal.classList.contains("hide")) {
     $modal.classList.remove("hide");
+    $video.pause();
+    $video.currentTime=0
+    $audio.play();
   }
 });
